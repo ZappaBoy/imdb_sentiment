6c8855d4888e40479cc086a20f26d27b6d9e9f5b
zappaboy <federico.zappone@justanother.cloud>
2023-12-19 03:30:45 +0100

    Partially fixed pytorch model;

--- imdb_sentiment/analyzer.py
+++ imdb_sentiment/analyzer.py
@@ -2,17 +2,23 @@ import importlib.metadata as metadata
 
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 import tensorflow_datasets as tfds
+import torch
 from transformers import Trainer, AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, \
     DataCollatorWithPadding, EarlyStoppingCallback
 
 from imdb_sentiment.shared.utils.logger import Logger
 from shared.utils.custom_dataset import CustomDataset
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+torch.cuda.empty_cache()
+
 max_tokenization_length = 512
-
 __version__ = metadata.version(__package__ or __name__)
-
 tokenized_args = {
     'padding': True,
     'truncation': True,
@@ -30,9 +36,9 @@ class Analyzer:
     def run(self):
         self.logger.info(f"Analyzing")
         train_dataset, test_dataset = self.get_dataset()
-        self.pretrain(train_dataset, test_dataset)
-        self.model.save_pretrained("pretrained-model")
-        self.tokenizer.save_pretrained("pretrained-model")
+        # self.pretrain(train_dataset, test_dataset)
+        # self.model.save_pretrained("pretrained-model")
+        # self.tokenizer.save_pretrained("pretrained-model")
 
         val_trainer = Trainer(self.model)
         test_dataset = test_dataset[:10]
@@ -40,12 +46,13 @@ class Analyzer:
         val_label = list(test_dataset['label'])
         val_dataset = CustomDataset(val_text_tokenized, val_label)
         correct = 0
-        raw_pred, _, _ = val_trainer.predict(val_dataset)
+        # TODO: Fix this
+        raw_pred, correct_pred, metrics = val_trainer.predict(val_dataset)
 
         y_pred = np.argmax(raw_pred, axis=1)
-        for index, (label, text) in enumerate(test_dataset.values):
-            print(index, label, y_pred[index])
-            if label == y_pred[index]:
+        for index, pred, correct_pred in enumerate(zip(y_pred, correct_pred)):
+            print(index, pred, correct_pred)
+            if pred == correct_pred:
                 print("Correct")
                 correct += 1
             else:
@@ -66,12 +73,16 @@ class Analyzer:
         test_dataset = CustomDataset(test_text_tokenized, test_label)
 
         training_args = TrainingArguments("test-trainer", evaluation_strategy="steps",
-                                          eval_steps=500,
-                                          per_device_train_batch_size=8,
-                                          per_device_eval_batch_size=8,
+                                          auto_find_batch_size=True,
+                                          eval_steps=30,
+                                          warmup_steps=2,
+                                          eval_accumulation_steps=1,
+                                          gradient_accumulation_steps=4,
+                                          gradient_checkpointing=True,
+                                          fp16=True,
                                           num_train_epochs=3,
                                           seed=0,
-                                          load_best_model_at_end=True, )
+                                          load_best_model_at_end=True)
 
         trainer = Trainer(
             model=self.model,
