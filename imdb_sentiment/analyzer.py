7942d2bfe5b22a9c50e442685b8ac34882d5ca69
zappaboy <federico.zappone@justanother.cloud>
2023-12-20 01:15:03 +0100

    Fixed pre-training model;

--- imdb_sentiment/analyzer.py
+++ imdb_sentiment/analyzer.py
@@ -1,4 +1,5 @@
 import importlib.metadata as metadata
+import os.path
 
 import numpy as np
 import pandas as pd
@@ -11,46 +12,71 @@ from transformers import Trainer, AutoTokenizer, AutoModelForSequenceClassificat
 from imdb_sentiment.shared.utils.logger import Logger
 from shared.utils.custom_dataset import CustomDataset
 
+__version__ = metadata.version(__package__ or __name__)
+
+# Disable GPU for Tensorflow
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.set_visible_devices([], 'GPU')
 
 torch.cuda.empty_cache()
 
 max_tokenization_length = 512
-__version__ = metadata.version(__package__ or __name__)
 tokenized_args = {
     'padding': True,
     'truncation': True,
     'max_length': max_tokenization_length
 }
 
+pretrained_model_path = 'pretrained-model'
+
+training_args = TrainingArguments("test-trainer", evaluation_strategy="steps",
+                                  auto_find_batch_size=True,
+                                  eval_steps=50,
+                                  warmup_steps=2,
+                                  eval_accumulation_steps=1,
+                                  gradient_accumulation_steps=4,
+                                  gradient_checkpointing=True,
+                                  fp16=True,
+                                  num_train_epochs=3,
+                                  seed=0,
+                                  load_best_model_at_end=True)
+
 
 class Analyzer:
     def __init__(self):
         self.logger = Logger()
+        self.model = None
+        self.tokenizer = None
         model_name = 'siebert/sentiment-roberta-large-english'
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        if os.path.exists(pretrained_model_path):
+            self.load_model(pretrained_model_path, locally=True)
+        else:
+            self.load_model(model_name)
+
+    def load_model(self, model_name: str, locally: bool = False):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, local_files_only=locally)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, local_files_only=locally)
 
     def run(self):
         self.logger.info(f"Analyzing")
         train_dataset, test_dataset = self.get_dataset()
-        # self.pretrain(train_dataset, test_dataset)
-        # self.model.save_pretrained("pretrained-model")
-        # self.tokenizer.save_pretrained("pretrained-model")
+        if not os.path.exists(pretrained_model_path):
+            self.pretrain(train_dataset, test_dataset)
+            self.model.save_pretrained("pretrained-model")
+            self.tokenizer.save_pretrained("pretrained-model")
 
-        val_trainer = Trainer(self.model)
-        test_dataset = test_dataset[:10]
+        data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)
+        val_trainer = Trainer(model=self.model, args=training_args, tokenizer=self.tokenizer,
+                              data_collator=data_collator)
         val_text_tokenized = self.tokenizer(list(test_dataset['text']), **tokenized_args)
         val_label = list(test_dataset['label'])
         val_dataset = CustomDataset(val_text_tokenized, val_label)
         correct = 0
-        # TODO: Fix this
         raw_pred, correct_pred, metrics = val_trainer.predict(val_dataset)
 
         y_pred = np.argmax(raw_pred, axis=1)
-        for index, pred, correct_pred in enumerate(zip(y_pred, correct_pred)):
+        for index, (pred, correct_pred) in enumerate(zip(y_pred, correct_pred)):
             print(index, pred, correct_pred)
             if pred == correct_pred:
                 print("Correct")
@@ -72,18 +98,6 @@ class Analyzer:
         train_dataset = CustomDataset(train_text_tokenized, train_label)
         test_dataset = CustomDataset(test_text_tokenized, test_label)
 
-        training_args = TrainingArguments("test-trainer", evaluation_strategy="steps",
-                                          auto_find_batch_size=True,
-                                          eval_steps=30,
-                                          warmup_steps=2,
-                                          eval_accumulation_steps=1,
-                                          gradient_accumulation_steps=4,
-                                          gradient_checkpointing=True,
-                                          fp16=True,
-                                          num_train_epochs=3,
-                                          seed=0,
-                                          load_best_model_at_end=True)
-
         trainer = Trainer(
             model=self.model,
             args=training_args,
@@ -100,6 +114,7 @@ class Analyzer:
     def get_dataset() -> (pd.DataFrame, pd.DataFrame):
         dataset, info = tfds.load('imdb_reviews/plain_text', with_info=True, as_supervised=True)
         train_dataset, test_dataset = dataset['train'], dataset['test']
+        # Remove duplicates
         train_df = tfds.as_dataframe(train_dataset, info)
         test_df = tfds.as_dataframe(test_dataset, info)
         train_df['text'] = train_df['text'].astype(str)
